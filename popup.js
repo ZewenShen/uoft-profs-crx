ab0e39676b3221b311d2054da99c45056662c10b
Zewen <41880356@qq.com>
2018-06-16 13:26:12 +0800

    Simplest version: Add the search bar to the extension

---
+++ popup.js
@@ -0,0 +1,30 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.create({'url': chrome.extension.getURL('open_page_name.html')}, function(tab) {
+        //tab opened
+    });
+});
+function encodeQueryData(data) {
+   let ret = [];
+   for (let d in data)
+     ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
+   return ret.join('&');
+}
+$(function(){
+	$('#btn').click(function(){
+		instructor = $('#inlineFormInputName').val();
+		course = $('#inlineFormInputGroupUsername').val();
+		campus = $('#university').val();
+		data = {"instructor": instructor, "course": course, "campus": campus};
+		if (instructor == undefined || course == undefined || !/^[A-Za-z\s]*$/.test(instructor) || !/^[A-Z]{3}\d?\d?\d?$/.test(course) || (campus != 'St. George' && campus != 'Mississauga')) {
+			var notifOptions = {
+				type: 'basic',
+				iconUrl: 'icon48.png',
+				title: "Error Message from UofT Profs",
+				message: "The content that you submit doesn't satisfy the requirement!"
+			};
+			chrome.notifications.create('limitNotif', notifOptions);
+		} else {
+			chrome.tabs.create({'url': 'http://172.105.219.80:3000/profanalysis?' + encodeQueryData(data)});
+		}
+	});
+});

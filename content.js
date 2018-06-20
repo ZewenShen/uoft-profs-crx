dff528bdb217298131e11205c9cfe3ac10037b22
Zewen <41880356@qq.com>
2018-06-21 00:12:24 +0800

    add hyperlink to dialog

--- content.js
+++ content.js
@@ -1,6 +1,11 @@
 setInterval(function() {
+	setCourseList();
+	setCourseDialog();
+}, 1000);
+
+function setCourseList() {
 	$('.courseBox.courseEnrolment').each(function() {
-		var courseInfoRawString = $('.enrolment-code', $(this)).text();
+		var courseInfoRawString = $('.enrolment-code', $(this)).text().trim();
 		var courseIDFull = courseInfoRawString.split(' ')[0];
 		var courseID = courseIDFull.substring(0, courseIDFull.length - 2);
 		var campusID = courseIDFull[courseIDFull.length - 1];
@@ -8,9 +13,24 @@ setInterval(function() {
 		var instructor = instructorSelector.html();
 		if (instructor.indexOf('href') == -1 && /^[A-Z]{3}\d{3}$/.test(courseID)) {
 			var campus = (campusID == 1) ? "St. George": "Mississauga";
-			console.log("<a href='" + encodeURI("http://uoftprofs.com/?" + "instructor=" + instructor + "&course=" + courseID + "&campus=" + campus) + "'>" + instructor + "</a>");
+			// console.log("<a href='" + encodeURI("http://uoftprofs.com/?" + "instructor=" + instructor + "&course=" + courseID + "&campus=" + campus) + "'>" + instructor + "</a>");
     		instructorSelector.html("<a href='" + encodeURI("http://uoftprofs.com/?" + "instructor=" + instructor + "&course=" + courseID + "&campus=" + campus) + "'>" + instructor + "</a>");
 		}
 	});
-}, 1000);
+}
 
+function setCourseDialog() {
+	var courseInfoRawString = $('#courseCodeAndTitle').text().trim();
+	var courseIDFull = courseInfoRawString.split(' ')[0];
+	var courseID = courseIDFull.substring(0, courseIDFull.length - 2);
+	var campusID = courseIDFull[courseIDFull.length - 1];
+	$('.searchResults > .primaryActivity > .activityRow > .instructor > .instructorDetails').each(function() {
+		var instructor = $(this).html();
+		if (instructor.indexOf('href') == -1 && /^[A-Z]{3}\d{3}$/.test(courseID)) {
+			var campus = (campusID == 1) ? "St. George": "Mississauga";
+			// console.log("<a href='" + encodeURI("http://uoftprofs.com/?" + "instructor=" + instructor + "&course=" + courseID + "&campus=" + campus) + "'>" + instructor + "</a>");
+    		$(this).html("<a href='" + encodeURI("http://uoftprofs.com/?" + "instructor=" + instructor + "&course=" + courseID + "&campus=" + campus) + "'>" + instructor + "</a>");
+		}
+	});
+
+}

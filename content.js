1b7a72e5728df99084e3badca4584d6cdcf877cf
Zewen <41880356@qq.com>
2018-06-29 12:11:14 +0800

    Now it supports fall/winter

--- content.js
+++ content.js
@@ -6,13 +6,14 @@ $('*').on('click', function() {
 		return;
 	}
 	setCourseList();
-	setCourseDialog();
+	setCourseDialogFallWinter();
+	setCourseDialogSummer();
 	}, 500);
 });
 
 
 function setCourseList() {
-	$('.courseBox.courseEnrolment').each(function() {
+	$('.courseBox').each(function() {
 		var courseInfoRawString = $('.enrolment-code', $(this)).text().trim();
 		var courseIDFull = courseInfoRawString.split(' ')[0];
 		var courseID = courseIDFull.substring(0, courseIDFull.length - 2);
@@ -31,8 +32,26 @@ function setCourseList() {
 	});
 }
 
-function setCourseDialog() {
+function setCourseDialogSummer() {
 	var courseInfoRawString = $('#courseCodeAndTitle').text().trim();
+	var courseIDFull = courseInfoRawString.split(' ')[0];
+	var courseID = courseIDFull.substring(0, courseIDFull.length - 2);
+	var campusID = courseIDFull[courseIDFull.length - 1];
+	$('.searchResults > .primaryActivity > .activityRow > .instructor > .instructorDetails').each(function() {
+		var instructors = $(this).html();
+		if (instructors.indexOf('href') == -1 && instructors != "TBA" && /^[A-Z]{3}\d{3}$/.test(courseID)) {
+			var campus = (campusID == 1) ? "St. George": "Mississauga";
+			var instructorList = instructors.split(', ');
+			var aArray = new Array(instructorList.length);
+			instructorList.forEach(function(instructor) {
+				aArray.push("<a href='" + encodeURI("http://uoftprofs.com/?" + "instructor=" + instructor + "&course=" + courseID + "&campus=" + campus) + "'>" + instructor + "</a>");
+			});
+    		$(this).html(aArray.reduce(function(x, y) {return x + ', ' + y;}));
+		}	
+	});
+}
+function setCourseDialogFallWinter() {
+	var courseInfoRawString = $('#modalHeading').prop("lastChild").nodeValue.trim();
 	var courseIDFull = courseInfoRawString.split(' ')[0];
 	var courseID = courseIDFull.substring(0, courseIDFull.length - 2);
 	var campusID = courseIDFull[courseIDFull.length - 1];
